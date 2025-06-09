1df375db09c2695189706c0d3fac92fbf3194c8d
AL AMIN <alaminmd@stu.scu.edu.cn>
2025-06-09 09:18:01 +0600

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import base64
+import os
 import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -7,11 +9,21 @@ app = Flask(__name__)
 
 # Google Sheets API Setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)  # Use the correct file name
+
+# Get the environment variable for credentials (Base64 encoded)
+credentials_base64 = os.getenv("GOOGLE_CREDENTIALS")
+credentials_json = base64.b64decode(credentials_base64).decode('utf-8')
+
+# Save the decoded credentials to a temporary file
+with open('credentials.json', 'w') as f:
+    f.write(credentials_json)
+
+# Authorize the client using the credentials file
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
 client = gspread.authorize(creds)
 
-# Open the Google Sheet (Updated with your sheet URL)
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1iFpIAzz_tJ5s44Iuvy8Idcwwk6NfowdbNe4i66YzXdc/edit?usp=sharing#").sheet1
+# Open the Google Sheet
+sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1iFpIAzz_tJ5s44Iuvy8Idcwwk6NfowdbNe4i66YzXdc/edit?usp=sharing").sheet1
 
 @app.route('/')
 def home():
@@ -32,4 +44,4 @@ def submit_order():
         return f"Order received! Name: {name}, Size: {size}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
